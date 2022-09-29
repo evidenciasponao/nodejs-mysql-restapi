283c4ce21eacd3ef396e07d3dc50260717d1109d
evidenciasponao@gmail.com <evidenciasponao@gmail.com>
2022-09-29 19:21:04 +0200

    news

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express' // Importe express
 
-const app = express() // Inicio la aplicacion
+const app = express() // Inicio la aplicacions
 
 app.listen(3000) // Establezco el puerto de escucha
 console.log('Server running on port 3000') // Mensaje por console de la aplicacion
